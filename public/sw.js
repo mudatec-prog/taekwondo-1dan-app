5821e0afafbf94d6a35a26ba2333ad2ddc762519
mudatec-prog <mudatec@gmail.com>
2026-09-04 18:43:01 +0200

    Adjust Android app shell spacing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taekwondo-1dan-v3";
+const CACHE_NAME = "taekwondo-1dan-v5";
 const CORE_ASSETS = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
